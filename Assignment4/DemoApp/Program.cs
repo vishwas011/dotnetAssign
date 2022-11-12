db0e7ad4799911f43bd0a3a33981129a74fa56f7
vishwas011 <114655725+vishwas011@users.noreply.github.com>
2022-11-12 08:59:35 +0530

    Add files via upload

---
+++ Assignment4/DemoApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Edu;
+        
+	 double Compute(Sequence seq, int count) 
+      {
+		 if(seq is IResetable p)
+			 p.Reset();
+		 return seq.Sum(count)/count;
+	 }
+  
+   LinearSequence ls = new LinearSequence();
+   PowerSequence ps = new PowerSequence();
+  Console.WriteLine("The Sum of Linear Sequence is {0:0.00}",Compute(ls,12));
+  Console.WriteLine("The Sum of Power Sequence is {0:0.00}",Compute(ps,5));
